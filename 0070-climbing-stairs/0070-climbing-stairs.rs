c0041d4ecbc1bdc6f9ac545ca41a3c2775188c12
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-01 16:33:23 +0900

    Time: 0 ms (100.00%), Space: 2 MB (52.67%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let n = n as usize; 
+        
+        let mut dp = vec![0; n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        // Iterates from 2,3,4...n
+        for i in 2..(n+1) { 
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        dp[n]
+    }
+}
