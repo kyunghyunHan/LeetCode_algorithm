9c1bdc971a0423d6dc103d578729900533c88a4a
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-05-16 17:50:59 +0900

    Time: 0 ms (100%), Space: 2.1 MB (100%) - LeetHub

---
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.rs
@@ -0,0 +1,16 @@
+impl Solution {
+   pub fn is_strobogrammatic(num: String) -> bool {
+    let mut rotated = String::with_capacity(num.len());
+
+    for c in num.chars().rev() {
+        match c {
+            '0' | '1' | '8' => rotated.push(c),
+            '6' => rotated.push('9'),
+            '9' => rotated.push('6'),
+            _ => return false,
+        }
+    }
+
+    rotated == num
+}
+}
