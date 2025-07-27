3457599f5ba880660d5664497318b32c3d53215f
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-07-27 22:38:28 +1000

    Time: 0 ms (100%), Space: 2.2 MB (72%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rs
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rs
@@ -1,23 +1,37 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
 impl Solution {
-    pub fn merge_two_lists(
-        list1: Option<Box<ListNode>>, 
-        list2: Option<Box<ListNode>>
-    ) -> Option<Box<ListNode>> {
-        match (list1, list2) {
-            (Some(mut l1), Some(mut l2)) => {
-                if l1.val < l2.val {
-                    let next = Solution::merge_two_lists(l1.next.take(), Some(l2));
-                    l1.next = next;
-                    Some(l1)
-                } else {
-                    let next = Solution::merge_two_lists(Some(l1), l2.next.take());
-                    l2.next = next;
-                    Some(l2)
-                }
+pub fn merge_two_lists(
+    list1: Option<Box<ListNode>>,
+    list2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    match (list1, list2) {
+        (None, l2) => l2,
+        (l1, None) => l1,
+        (Some(mut l1_node), Some(mut l2_node)) => {
+            if l1_node.val < l2_node.val {
+                l1_node.next = Self::merge_two_lists(l1_node.next, Some(l2_node));
+                Some(l1_node)
+            } else {
+                l2_node.next =Self:: merge_two_lists(Some(l1_node), l2_node.next);
+                Some(l2_node)
             }
-            (Some(l1), None) => Some(l1),
-            (None, Some(l2)) => Some(l2),
-            (None, None) => None,
         }
     }
+}
+
 }
