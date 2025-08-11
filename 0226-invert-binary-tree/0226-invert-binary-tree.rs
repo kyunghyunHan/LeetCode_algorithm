ecd0cf99f746c2e46008f2980f9fc96ce54ed6a2
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-08-11 17:39:44 +1000

    Time: 0 ms (100%), Space: 2.1 MB (97.6%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.rs
@@ -0,0 +1,33 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = root.clone(){
+            let mut node_ref = node.borrow_mut();
+            let left = Self::invert_tree(node_ref.left.take());
+                  let right = Self::invert_tree(node_ref.right.take());
+
+            node_ref.left = right;
+            node_ref.right = left;
+        }
+        root
+    }
+}
