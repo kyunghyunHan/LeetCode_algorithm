7c8837dd30c319bb21a33ed1032a4b801a1e9a49
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-08-11 12:16:13 +1000

    Time: 164 ms (89.55%), Space: 0B (100%) - LeetHub

---
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -0,0 +1,12 @@
+SELECT 
+    e.employee_id,
+    t.team_size
+FROM Employee e
+JOIN (
+    SELECT 
+        team_id,
+        COUNT(*) AS team_size
+    FROM Employee
+    GROUP BY team_id
+) t
+ON e.team_id = t.team_id;
