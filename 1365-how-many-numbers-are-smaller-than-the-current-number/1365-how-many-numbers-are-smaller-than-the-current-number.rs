3a24fcbdfad60cb98bc236c07d35d94cb1f6eb5a
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-08-14 16:35:40 +1000

    Time: 1 ms (25.81%), Space: 2.3 MB (19.35%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
+     let mut result = Vec::new();
+    for i in 0..nums.len(){
+        let mut count = 0;
+
+        for j in 0..nums.len(){
+            if i ==j{
+                continue;
+            }
+            
+            if nums[i]>nums[j]{
+                count+=1;
+            }
+        }
+        result.push(count);
+    }
+    result
+    }
+}
