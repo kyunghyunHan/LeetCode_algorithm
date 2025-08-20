4025877f165fb8e5fd6d75bb25ffab459a64574f
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-08-21 09:57:12 +1000

    Time: 204 ms (96.67%), Space: 0B (100%) - LeetHub

---
+++ 2837-total-traveled-distance/2837-total-traveled-distance.sql
@@ -0,0 +1,9 @@
+SELECT 
+    u.user_id,
+    u.name,
+    COALESCE(SUM(r.distance), 0) AS "traveled distance"
+FROM Users u
+LEFT JOIN Rides r
+    ON u.user_id = r.user_id
+GROUP BY u.user_id, u.name
+ORDER BY u.user_id;
