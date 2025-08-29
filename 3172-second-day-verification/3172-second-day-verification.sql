50e7280270bcf1b2f60d447c3531c448cdcf9733
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-08-29 11:38:23 +1000

    Time: 205 ms (59.38%), Space: 0B (100%) - LeetHub

---
+++ 3172-second-day-verification/3172-second-day-verification.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT e.user_id
+FROM emails e
+JOIN texts t
+  ON e.email_id = t.email_id
+WHERE t.signup_action = 'Verified'
+  AND DATE(t.action_date) = DATE(e.signup_date + INTERVAL '1 DAY')
+ORDER BY e.user_id;
