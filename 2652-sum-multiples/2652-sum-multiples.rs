357e989fcc31227b296e1e7a0a6a1dd4ce1e7eac
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-09-17 11:26:24 +1000

    Time: 0 ms (100%), Space: 2.1 MB (93.33%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.rs
@@ -0,0 +1,9 @@
+use std::ops::RangeInclusive;
+impl Solution {
+    pub fn sum_of_multiples(n: i32) -> i32 {
+     let result= (1..=n)
+    .filter(|x| *x % 3 == 0 || *x % 5 == 0 || *x % 7 == 0)
+    .sum::<i32>(); 
+     result
+    }
+}
