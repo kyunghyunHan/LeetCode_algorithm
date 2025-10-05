6523c45d38108ac61de04a481f825b6630c12e44
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-10-05 22:56:50 +1100

    Time: 4 ms (12.5%), Space: 2.4 MB (0%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn max_product_difference(mut nums: Vec<i32>) -> i32 {
+        nums.sort_by(|a,b|a.cmp(&b));
+        
+        
+        
+        
+        
+        (nums[nums.len() -1 ] * nums[nums.len()-2]) - (nums[0] * nums[1])
+    }
+}
