884831f7e8a741d92e7318d0dbf05e337bd9c688
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-11-08 18:18:26 +1100

    Time: 0 ms (100%), Space: 2.1 MB (89.55%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+        let (mut num1 , mut num2) = (0,0);
+
+        for i in 1..=n{
+            if i % m ==0 {
+                num2+=i;
+            }else{
+                num1+=i;
+            }
+
+        }
+        num1 - num2
+    }
+}
