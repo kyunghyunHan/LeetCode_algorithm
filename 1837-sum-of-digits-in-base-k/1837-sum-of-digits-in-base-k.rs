536bca2fc7dda95754b391e553a8d96489cbd1d7
kyunhyun94 <ekfqlwcjswl@naver.com>
2026-01-20 22:19:24 +1100

    Time: 0 ms (100%), Space: 2.2 MB (40%) - LeetHub

---
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn sum_base(n: i32, k: i32) -> i32 {
+        let mut sum = 0;
+        let mut n = n;
+        while n > 0{
+            sum+=n%k;
+            n/=k;
+        }
+        sum
+    }
+
+
+}
